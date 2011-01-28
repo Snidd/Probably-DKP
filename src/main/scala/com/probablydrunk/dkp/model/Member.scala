ea421035dfd109e16be3ad112dc4a6d7b71472d6
Magnus Kjellberg <m.kjellberg@gmail.com>
2011-01-28 18:44:45 +0100

    Added Member

---
+++ src/main/scala/com/probablydrunk/dkp/model/Member.scala
@@ -0,0 +1,8 @@
+package com.probablydrunk.dkp.model
+import _root_.java.math.MathContext
+import _root_.net.liftweb.mapper._
+import _root_.net.liftweb.util.Empty
+
+class Member LongKeyedMapper[Account] with IdPK {
+	
+}
